4386b6bf1c5f9b33c3b1a77fc33d0d63455a1848
Dean Ruina <ruinadd@gmail.com>
2024-05-13 20:04:53 +0300

    access token exchange successful

--- backend/djangoserv/myapp/views.py
+++ backend/djangoserv/myapp/views.py
@@ -22,34 +22,35 @@ def initiate_oauth(request):
 
 
 def oauth_callback(request):
-    print(request.method)
     if request.method == 'GET':
         code = request.GET.get('code')
         state = request.GET.get('state')
         client_id = os.getenv("42_CLIENT_ID")
         client_secret = os.getenv("42_CLIENT_SECRET")  
+        
 
         # # Exchange the authorization code for an access token
-        # token_url = "https://api.intra.42.fr/oauth/token"
-        # payload = {
-        #     'grant_type': 'authorization_code',
-        #     'code': code,
-        #     'redirect_uri': redirect_uri,
-        #     'client_id': client_id,
-        #     'client_secret': client_secret,
-        # }
-        # response = requests.post(token_url, data=payload)
-        # if response.status_code == 200:
-        #     access_token = response.json().get('access_token')
+        token_url = "https://api.intra.42.fr/oauth/token"
+        data = {
+            'grant_type': 'authorization_code',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'code': code,
+            'redirect_uri': "http://127.0.0.1:8000/oauth_callback"
+        }
+        response = requests.post(token_url, data=data)
+        if response.status_code == 200:
+            access_token = response.json().get('access_token')
+            print(access_token)
         #     # Now you can use the access token to make authenticated requests on behalf of the user
         #     # Redirect the user to the desired page after successful login
-    return HttpResponseRedirect(reverse('home'))
-    #     else:
-    #         # Handle error
-    #         return HttpResponseRedirect(reverse('error'))
-    # else:
-    #     # If the request is not a GET request, redirect to the home page
-    #     return HttpResponseRedirect(reverse('home'))
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            # Handle error
+            return HttpResponseRedirect(reverse('error'))
+    else:
+        # If the request is not a GET request, redirect to the home page
+        return HttpResponseRedirect(reverse('home'))
 
 def error(request):
     return HttpResponse("An error occurred.", status=400)
