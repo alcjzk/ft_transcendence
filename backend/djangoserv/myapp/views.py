6c5c2894bd169eb74a20f823a0b0901da3563f6e
Dean Ruina <ruinadd@gmail.com>
2024-05-14 11:35:54 +0300

    single user authentication works, now adding another play

--- backend/djangoserv/myapp/views.py
+++ backend/djangoserv/myapp/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render, redirect
 from dotenv import load_dotenv
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.urls import reverse
 import requests
 import os
 import binascii
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'home.html')
 
 
 
-def initiate_oauth(request):
+def initiate_oauth(request: HttpRequest) -> HttpResponseRedirect:
     load_dotenv() # load .env to environment
     state = binascii.hexlify(os.urandom(16)).decode()  # To prevent CSRF attacks
     client_id = os.getenv("42_CLIENT_ID")
     redirect_uri = "http://127.0.0.1:8000/oauth_callback"  
     auth_url = f"https://api.intra.42.fr/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&state={state}&response_type=code"
+    request.session['oauth_state'] = state # Save state in session
     return redirect(auth_url)
 
 
 
-def oauth_callback(request):
+def oauth_callback(request: HttpRequest) -> HttpResponseRedirect:
     if request.method == 'GET':
+
         code = request.GET.get('code')
+        if not code or not code.isalnum():
+            return HttpResponseRedirect(reverse('error'), status=400)
+        
         state = request.GET.get('state')
+        if state is None or not state.isalnum():
+            return HttpResponseRedirect(reverse('error'), status=400)
+        
+        # Retrieve & compare state and session state
+        stored_state = request.session.get('oauth_state')
+        if state != stored_state:
+            return HttpResponseRedirect(reverse('error'), status=400)
+        
         client_id = os.getenv("42_CLIENT_ID")
         client_secret = os.getenv("42_CLIENT_SECRET")  
         
@@ -39,7 +52,11 @@ def oauth_callback(request):
         }
         response = requests.post(url ="https://api.intra.42.fr/oauth/token", data=data)
         if response.status_code == 200:
+            
             access_token = response.json().get('access_token')
+            if not access_token:
+                return HttpResponseRedirect(reverse('error'), status=401)
+            
             headers = {"Accept": "application/json", "Authorization": f"Bearer {access_token}"}
             response = requests.get(url="https://api.intra.42.fr/v2/me", headers=headers)
             if response.status_code == 200:
@@ -52,9 +69,16 @@ def oauth_callback(request):
                 print(usr1)
                 return HttpResponseRedirect(reverse('home'))
             
-        return HttpResponseRedirect(reverse('error'))
+        return HttpResponseRedirect(reverse('error'), status=401)
     else:
         return HttpResponseRedirect(reverse('home'))
 
-def  error(request):
-    return HttpResponse("An error occurred.", status=400)
+def error(status: int) -> HttpResponse:
+    if status == 400:
+        return HttpResponse("Bad Request. Status code: 400", status=status)
+    elif status == 401:
+        return HttpResponse("Unauthorized. Status code: 401", status=status)
+    elif status == 500:
+        return HttpResponse("Internal Server Error. Status code: 500", status=status)
+    else:
+        return HttpResponse(f"An error occurred. Status code: {status}", status=status)
