f7e9ca7e4db9f263f408ec3049c4b46cf7317e5e
lsileoni <lsileoni@proton.me>
2024-08-23 20:45:42 +0300

    Fix alertmanager issue

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -5,4 +5,4 @@ sed -i "s/__SMTP_SENDER__/${SMTP_SENDER}/" /etc/alertmanager/alertmanager.yml
 sed -i "s/__SMTP_RECEIVER__/${SMTP_RECEIVER}/" /etc/alertmanager/alertmanager.yml
 sed -i "s/__SMTP_HOST__/${SMTP_HOST}/" /etc/alertmanager/alertmanager.yml
 
-/alertmanager/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
+exec /alertmanager/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
